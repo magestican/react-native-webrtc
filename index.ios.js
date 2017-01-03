be3ae18c694cde93c563c81de941feb2ea2315fb
Bryan Arbelo <magestican.visualkei@gmail.com>
2017-01-04 10:31:57 +1100

    WORKS ON IOS

--- index.ios.js
+++ index.ios.js
@@ -1,19 +1,5 @@
-import {Provider} from 'react-redux';
-import store from './src/redux/store';
-import AppViewContainer from './src/modules/AppViewContainer';
-
-import React from 'react';
-import {AppRegistry} from 'react-native';
-
-const PepperoniAppTemplate = React.createClass({
-
-  render() {
-    return (
-      <Provider store={store}>
-        <AppViewContainer />
-      </Provider>
-    );
-  }
-});
-
-AppRegistry.registerComponent('PepperoniAppTemplate', () => PepperoniAppTemplate);
+var window = global || window;
+window.navigator = {
+  userAgent :  "react-native"
+}
+require('./src/components/main');
