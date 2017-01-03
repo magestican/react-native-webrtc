a510806580b31eac50017361dfa63e009bd3403c
Bryan Arbelo <magestican.visualkei@gmail.com>
2017-01-03 14:56:24 +1100

    WORKING ANDROID VERSION

--- android/app/src/main/java/com/pepperoniapptemplate/MainApplication.java
+++ android/app/src/main/java/com/pepperoniapptemplate/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.oney.WebRTCModule.WebRTCModulePackage;  // <--- Add this line
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,7 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
-        new VectorIconsPackage()
+        new VectorIconsPackage(),
+        new WebRTCModulePackage()                  // <--- Add this line
       );
     }
   };
